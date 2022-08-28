1c4a703aa4f547dff885faa83b68735c1769d169
Fouad Atwi <atwe98@gmail.com>
2022-08-28 18:20:32 +0200

    Fixed formatting for window size

--- sync.sh
+++ sync.sh
@@ -2,15 +2,16 @@
 
 # variables
 required_width=80
-required_height=35
+required_height=34
 
 check_terminal_size() {
     # Using tput to get terminal dimensions
     lines=$(tput lines)
     columns=$(tput cols)
+
     # Check if terminal is large enough. If not, exit with error 1
     if (($lines < $required_height || $columns < $required_width)); then
-        echo -e "The terminal must have a dimension of at least $required_width"x"$required_height\nThe current size is $lines $columns" 1>&2
+        echo -e "The terminal must have a dimension of at least $required_width"x"$required_height\nThe current size is $columns"x"$lines" 1>&2
         exit 1
     fi
 }
