fd6c021c8d9cca05b0cde28c2d9c82cb50a59ab8
Fouad Atwi <atwe98@gmail.com>
2022-08-29 17:27:32 +0200

    Fixed dynamic world creation
    The code can now generate a world using a blueprint
    The blueprint is represented by the variable map

--- sync.sh
+++ sync.sh
@@ -10,77 +10,113 @@ player_pos=(16 78)
 declare -a map=()
 # Below is the variable map which represents the map of the game. Modify the below array to change the map.
 # Empty spaces are represented by ' ' and walls are represented by 'W'.
-# The outer walls of the map are not represented.
-# The size of the map is 78x32
-map+=("                                                                              ")
-map+=("                                                                              ")
-map+=("                                                                              ")
-map+=("                                                                              ")
-map+=("                                                                              ")
-map+=("WWWWWWWWW    WWWWWWWWWW    WWWWWWWWWW    WWWWWWWWWW    WWWWWWWWWW    WWWWWWWWW")
-map+=("                                                                              ")
-map+=("                                                                              ")
-map+=("                                                                              ")
-map+=("     WWWWWWWWWWWW                WWWWWWWWWWWW                WWWWWWWWWWWW     ")
-map+=("     W          W                W          W                W          W     ")
-map+=("     W          W                W          W                W          W     ")
-map+=("     W          W                W          W                W          W     ")
-map+=("     WWWWWWWWWWWW                WWWWWWWWWWWW                WWWWWWWWWWWW     ")
-map+=("                                                                              ")
-map+=("                                                                              ")
-map+=("                                                                              ")
-map+=("                                                                              ")
-map+=("     WWWWWWWWWWWW                WWWWWWWWWWWW                WWWWWWWWWWWW     ")
-map+=("     W          W                W          W                W          W     ")
-map+=("     W          W                W          W                W          W     ")
-map+=("     W          W                W          W                W          W     ")
-map+=("     WWWWWWWWWWWW                WWWWWWWWWWWW                WWWWWWWWWWWW     ")
-map+=("                                                                              ")
-map+=("                                                                              ")
-map+=("                                                                              ")
-map+=("WWWWWWWWW    WWWWWWWWWW    WWWWWWWWWW    WWWWWWWWWW    WWWWWWWWWW    WWWWWWWWW")
-map+=("                                                                              ")
-map+=("                                                                              ")
-map+=("                                                                              ")
-map+=("                                                                              ")
-map+=("                                                                              ")
+# The size of the default map is 80x34
+map+=("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW")
+map+=("W                                                                              W")
+map+=("W                                                                              W")
+map+=("W                                                                              W")
+map+=("W                                                                              W")
+map+=("W                                                                              W")
+map+=("WWWWWWWWWW    WWWWWWWWWW    WWWWWWWWWW    WWWWWWWWWW    WWWWWWWWWW    WWWWWWWWWW")
+map+=("W                                                                              W")
+map+=("W                                                                              W")
+map+=("W                                                                              W")
+map+=("W     WWWWWWWWWWWW                WWWWWWWWWWWW                WWWWWWWWWWWW     W")
+map+=("W     W          W                W          W                W          W     W")
+map+=("W     W          W                W          W                W          W     W")
+map+=("W     W          W                W          W                W          W     W")
+map+=("W     WWWWWWWWWWWW                WWWWWWWWWWWW                WWWWWWWWWWWW     W")
+map+=("W                                                                              W")
+map+=("W                                                                              W")
+map+=("W                                                                              W")
+map+=("W                                                                              W")
+map+=("W     WWWWWWWWWWWW                WWWWWWWWWWWW                WWWWWWWWWWWW     W")
+map+=("W     W          W                W          W                W          W     W")
+map+=("W     W          W                W          W                W          W     W")
+map+=("W     W          W                W          W                W          W     W")
+map+=("W     WWWWWWWWWWWW                WWWWWWWWWWWW                WWWWWWWWWWWW     W")
+map+=("W                                                                              W")
+map+=("W                                                                              W")
+map+=("W                                                                              W")
+map+=("WWWWWWWWWW    WWWWWWWWWW    WWWWWWWWWW    WWWWWWWWWW    WWWWWWWWWW    WWWWWWWWWW")
+map+=("W                                                                              W")
+map+=("W                                                                              W")
+map+=("W                                                                              W")
+map+=("W                                                                              W")
+map+=("W                                                                              W")
+map+=("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW")
 
-world_template="\
-V\
-H╔══════════════════════════════════════════════════════════════════════════════╗\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H║m║\\n\
-H╚══════════════════════════════════════════════════════════════════════════════╝"
+declare -A walls
+walls["ud"]="║"
+walls["u"]="║"
+walls["d"]="║"
+walls["lr"]="═"
+walls["l"]="═"
+walls["r"]="═"
+walls["ur"]="╚"
+walls["ul"]="╝"
+walls["dr"]="╔"
+walls["dl"]="╗"
+walls["udlr"]="╬"
+walls["ulr"]="╩"
+walls["dlr"]="╦"
+walls["udr"]="╠"
+walls["udl"]="╣"
+walls["none"]="█"
+
+build_walls() {
+    local _arr="$1"
+    local -n _arr_ref="$1"
+    local -a built_map=("${_arr_ref[@]}")
+    for ((row = 0; row < "${#_arr_ref[@]}"; row++)); do
+        for ((col = 0; col < "${#_arr_ref[$row]}"; col++)); do
+            local element="$(get_element_at_position "$_arr" $row $col)"
+
+            if [[ "$element" != "W" ]]; then
+                continue
+            fi
+
+            local wall=""
+
+            if ((row > 0)); then
+                local up="$(get_element_at_position "$_arr" $((row - 1)) $col)"
+                if [[ "$up" == "W" ]]; then
+                    wall+='u'
+                fi
+            fi
+
+            if ((row < "${#_arr_ref[@]}" - 1)); then
+                local down="$(get_element_at_position "$_arr" $((row + 1)) $col)"
+                if [[ "$down" == "W" ]]; then
+                    wall+='d'
+                fi
+            fi
+
+            if ((col > 0)); then
+                local left="$(get_element_at_position "$_arr" $row $((col - 1)))"
+                if [[ "$left" == "W" ]]; then
+                    wall+='l'
+                fi
+            fi
+
+            if ((col < "${#_arr_ref[$row]}" - 1)); then
+                local right="$(get_element_at_position "$_arr" $row $((col + 1)))"
+                if [[ "$right" == "W" ]]; then
+                    wall+='r'
+                fi
+            fi
+
+            if [[ -z "$wall" ]]; then
+                wall="none"
+            fi
+
+            replace_element_at_position built_map $row $col "${walls[$wall]}"
+        done
+    done
+    echo done
+    map=("${built_map[@]}")
+    # echo "${map[1]}"
+}
 
 check_terminal_size() {
     # Using tput to get terminal dimensions
@@ -124,7 +160,7 @@ get_element_from_row() {
     local y=$2
     # Get the element at the given position
     local element="${row:$y:1}"
-    echo -en "$element"
+    echo -e "$element"
 }
 
 get_element_at_position() {
@@ -133,26 +169,66 @@ get_element_at_position() {
     local -n _arr_ref="$1"
     local x=$2
     local y=$3
-    local row="$(get_row $_arr $x)"
-    local element="$(get_element_from_row "$row" $y)"
-    echo -en "$element"
+    local element="${_arr_ref[$x]:$y:1}"
+    echo -e "$element"
 }
 
 update_world() {
+    local _arr="$1"
+    local -n _arr_ref="$1"
+    local world_template="\
+V\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm\\n\
+Hm"
     local world_temp=$world_template
-    for row in "${map[@]}"; do
+    for row in "${_arr_ref[@]}"; do
+
         world_temp=$(echo -en "$world_temp" | sed "0,/m/{s/m/$row/}")
     done
     world=$(echo -en "$world_temp" | sed "s/H/$h_separator/g" | sed "s/V/$v_separator/g")
 }
 
 print_world() {
+    local _arr="$1"
+    local -n _arr_ref="$1"
     if [[ size_ok -eq 0 ]]; then
         return
     fi
     # Clear the terminal
     clear
-    update_world
+    update_world "$_arr"
     # Print the world
     echo -en "$world"
 }
@@ -165,10 +241,10 @@ replace_element_at_position() {
     local y=$3
     local new_element=$4
     local row="$(get_row $_arr $x)"
-    local element=$(get_element_from_row $row $y)
+    local element=$(get_element_from_row "$row" $y)
     local new_row=${row:0:$3}$new_element${row:$3+1}
     _arr_ref[$x]=$new_row
-    update_world
+    update_world "$_arr"
 }
 
 replace_element_at_position_if_possible() {
@@ -185,18 +261,17 @@ replace_element_at_position_if_possible() {
     else
         echo -e "The position is already occupied" 1>&2
     fi
-    update_world
+    update_world "$_arr"
 }
 
 main() {
-    trap "check_terminal_size;print_world" SIGWINCH
+    trap "check_terminal_size;print_world map" SIGWINCH
     # Call the functions
-    check_terminal_size
-    update_world
-    # place_object 2 0 ^
-    # get_row map 5
-    replace_element_at_position_if_possible map 4 0 ^
 
+    check_terminal_size
+    build_walls map
+    update_world map
+    print_world map
 
     while :; do
         sleep .1
